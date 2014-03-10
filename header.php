7f9d4c00ab716b6c5733fed70cd6f9af6e76557d
Semiomchik <sergio.semenyuk@gmail.com>
2014-03-10 22:52:23 +0200

    Update header.php

--- header.php
+++ header.php
@@ -5,6 +5,6 @@ include_once 'lib/Db/db_class.php';
 <!doctype html>
 <html lang="en">
 <head>
-    <link href="/./styles/styles.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="/./scripts/mootools-core-1.4.5-full-compat-nc.js"></script>
-    <script type="text/javascript" src="/./scripts/script.js"></script>
+    <link href="styles/styles.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="scripts/mootools-core-1.4.5-full-compat-nc.js"></script>
+    <script type="text/javascript" src="scripts/script.js"></script>
